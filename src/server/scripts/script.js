6ebfd983c1f2172e9843ade3f443e06aa904d971
saelak <saad.el-abdellaoui@polymtl.ca>
2018-12-01 18:41:59 -0500

    removed a folder

---
+++ src/server/scripts/script.js
@@ -0,0 +1,71 @@
+
+function getAll() {
+    $.get("http://localhost:8080/api/bills/", function(data, status) {
+        $('#responseBody').html(JSON.stringify(data));
+        $('#responseStatus').html(status);
+    });
+} 
+
+function getBill() {
+    
+    $.get("http://localhost:8080/api/bills/" + $("#idInput").val(), function(data, status) {
+        $('#responseBody').html(JSON.stringify(data));
+        $('#responseStatus').html(status);
+    }).fail(function(xhr, status, error) {
+        $('#responseStatus').html(status);
+        $('#responseBody').html(error);
+    });    
+}
+
+function postBill() {
+    var input = JSON.parse($("#body").val());
+    if(input.id === undefined || input.id === null || input.totalPrice === undefined || input.products === undefined || input.products === [])
+    {
+        return;
+    }
+
+    var realInput = JSON.stringify(input);
+
+   $.ajax({
+        type:'POST',
+        url: "http://localhost:8080/api/bills/",
+        contentType: 'application/json',
+        data: realInput,
+        success: (msg) => {
+            $('#responseBody').html(JSON.stringify(msg));
+        },
+        fail: (xhr, status, error) =>{
+            $('#responseStatus').html(status);
+            $('#responseBody').html(error);
+        }
+    });
+}
+
+function deleteAll() {    
+    $.ajax({
+        type:'DELETE',
+        url: "http://localhost:8080/api/bills/",
+        success: (msg) => {
+            $('#responseStatus').html("All bills deleted!");
+        },
+        fail: (xhr, status, error) =>{
+            $('#responseStatus').html(status);
+            $('#responseBody').html(error);
+        }
+    });
+}
+
+function deleteBill() {
+    
+    $.ajax({
+        type:'DELETE',
+        url: "http://localhost:8080/api/bills/" + $("#idInput").val() ,
+        success: (msg) => {
+            $('#responseStatus').html("Bill " + $("#idInput").val() + " deleted!");
+        },
+        fail: (xhr, status, error) =>{
+            $('#responseStatus').html(status);
+            $('#responseBody').html(error);
+        }
+    });
+}
